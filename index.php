826f7f26b36266cb6c3901ff0ab4eb09048f533d
sagar969696 <60964667+sagar969696@users.noreply.github.com>
2020-12-22 18:17:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,277 @@
+<html>
+    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+         <?php $n=$_GET["n"]; ?>
+    
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
+    <title>Merry Christmas Greetings with Name</title>
+    <meta name="google" value="notranslate">
+    <meta property="og:type" content="Greetings" />
+    <meta property="og:title" content="Merry Christmas Greetings with Name" />
+    <meta property="og:url" content="http://christmas.sarkarispy.com">
+    <meta property="og:description" content="Xmas 2018 - Sent Animated & beautiful Merry Christmas greeting card with your name to friends & relatives" />
+    <meta property="og:site_name" content="http://christmas.sarkarispy.com" />
+    <meta property="og:image" content="img/gift.webp" />
+    <meta property="og:image:secure_url" content="img/gift.webp" />
+    <meta property="og:image:width" content="993" />
+    <meta property="og:image:height" content="559" />
+    <meta property="og:image:alt" content="Merry Christmas Greetings with Name" />
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link href='https://fonts.googleapis.com/css?family=Sofia:&effect=neon' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/wp.webp">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script>
+ if ('serviceWorker' in navigator) {
+
+    console.log("Will the service worker register?");
+
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(reg){
+
+        console.log("Yes, it did.");
+
+     }).catch(function(err) {
+
+        console.log("No it didn't. This happened:", err)
+
+    });
+ }
+
+
+</script>
+</head>
+<iframe width="0" height="0" src="song.mp3" frameborder="0" allowfullscreen></iframe>
+<body class="bg" id="rakhi" style="background-image: linear-gradient(to bottom, #ff0000, #ff517f, #ff97d2, #f8d1fc, #ffffff););
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;">
+
+
+
+
+</center>
+<div class="container">
+  <marquee class="m1" behavior="scroll" direction="up" scrolldelay="5">  <br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+
+</marquee>
+<marquee class="m2" behavior="scroll" direction="down" scrolldelay="5"><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+</marquee>
+<script src="js/doodle.js"></script> 
+<script type="text/javascript">
+ doodle.init("img/fly.webp");
+</script>
+<div class="container" >
+    <div class="main-greeting" >
+<div align="center html2canvas-ignore">
+     <div style="font-size: 17px; font-weight: 800; color: white;" >
+<center>
+    <p id="demo"></p></center>
+
+            <div class="main_body" >
+      <center>
+<figure>
+    
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+
+
+</figure><br> <br><br><br>
+</center>
+<p id="demo"></p> 
+<center>
+<img src="https://i.imgur.com/DPMaeP8.png" width="70%" height="13%"style="animation: tada 4s infinite"><br>
+<img src="img/santa.webp" width="350px" height="400px"style="animation: pulse 1.5s infinite"><br><br>
+</center>
+<center>
+<span class="hny-txt" style="color: #000">
+<span style="font-size: 28px;">Merry Christmas</span><br />
+
+Me: “I want a magical Unicorn for Christmas”<br />
+Santa: “Be realistic”<br />
+Me: “Okay. I want 5 minutes to myself each day to drink my coffee hot and pee in peace.”<br />
+Santa: “What color Unicorn would you like?”<br />
+This made me think of us! Thank you for being such a great friend and support this year. Wishing you more sleep and time for yourself in the coming year. Happy New Year!<br />
+</span></p>
+<img src="1.gif"  class="swing2"  height="300px" width="300px"/>
+</center>
+<center>
+<p class="hny-txt">
+  <span style="font-size: 32px;"><b>X-Mas Gift By</span> <span style="color: #008000;"></span></p></center>&nbsp;
+<br>
+<center>
+   <!-- Display the countdown timer in an element -->
+
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("dec 25, 2020 00:00:00").getTime();
+
+// Update the count down every 01 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "<i> Days,</i> " + hours + " <i>Hrs,</i> "
+  + minutes + "<i> Min,</i> " + seconds + "<i> Sec</i> ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Merry Christmas";
+  }
+}, 1000);
+</script> 
+</div>
+<center>
+
+<div class="button">
+    
+                
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</div></center><br>
+<center>
+
+
+</center>
+
+
+<br><br>
+</div>
+</div>
+</div>
+</div>
+</div>
+<form method="get" action="share.php">
+  <div class="enter-name">
+    <input class="animated pulse infinite" type="name" required="" maxlength="50" name="n" placeholder="👉 Enter Your Name Here">
+    <button class="btn animated shake infinite" type="submit"><span>👉</span> Go</button>
+  </div>
+</form>       
+<canvas id="canvas" style="position : absolute; top : 0px; left : 0px;"></canvas>
+    </body>
+    <link rel="manifest" href="js/manifest.json">
+</html> 
+
---
+++ share.php
@@ -0,0 +1,298 @@
+<html>
+    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+         <?php $n=$_GET["n"]; ?>
+    
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
+    <title>Share Christmas Wishes with Name on WhatsApp</title>
+    <meta name="google" value="notranslate">
+    <meta property="og:type" content="Greetings" />
+    <meta property="og:title" content="Merry Christmas Greetings with Name" />
+    <meta property="og:url" content="http://christmas.sarkarispy.com">
+    <meta property="og:description" content="Share Christmas Greetings with Name on WhatsApp" />
+    <meta property="og:site_name" content="christmas.sarkarispy.com" />
+    <meta property="og:image" content="img/gift.webp" />
+    <meta property="og:image:secure_url" content="img/gift.webp" />
+    <meta property="og:image:width" content="993" />
+    <meta property="og:image:height" content="559" />
+    <meta property="og:image:alt" content="Merry Christmas Greetings with Name" />
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link href='https://fonts.googleapis.com/css?family=Sofia:&effect=neon' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/wp.webp">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <style>
+        .footerbtn {
+ 
+            display: block;
+            line-height: 15px;
+            position: fixed;
+            left:0px;
+            bottom:0px;
+            height:40px;
+            
+border-radius: 15px;
+  box-sizing: border-box;
+  padding: 5px;
+  background:#34af23;
+  color: #ffffff;
+  font-size: 18px;
+  text-align: center;
+  text-decoration: none;
+  width:95%;
+ margin-left:10px;
+            margin-right:30px;
+            box-shadow: 0 4px 12px 0 rgba(0, 0, 0, .3);
+            animation: footer infinite linear 1s;
+            -webkit-transform: translate3d(30%,0,0);
+            transform: translate3d(30%,0,0);
+            position: fixed;
+           
+}
+
+.footerbtn :active {
+            box-shadow: none
+        }
+
+        @-webkit-keyframes footer {
+            from {
+                -webkit-transform: rotateZ(0)
+            }
+            25% {
+                -webkit-transform: rotateZ(1.5deg)
+            }
+            50% {
+                -webkit-transform: rotateZ(0deg)
+            }
+            75% {
+                -webkit-transform: rotateZ(-1.5deg)
+            }
+            to {
+                -webkit-transform: rotateZ(0)
+            }}
+        
+    </style>
+    
+</head>
+
+<body class="bg" id="rakhi" style="background: linear-gradient(to bottom right, #ff0c45, #ffffff, #edff2d);
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;">
+<center><img src="https://goo.gl/xvjgXx" width="100%" height="12%" alt="A very merry Christmas and happy new year 2019"></center>
+<center>
+
+
+</center>
+<div class="container">
+  <marquee class="m1" behavior="scroll" direction="up" scrolldelay="5">  <br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+
+</marquee>
+<marquee class="m2" behavior="scroll" direction="down" scrolldelay="5"><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+<img src="s.png" height="119px" width="35px"/><br><br>
+</marquee>
+<div class="container" >
+    <div class="main-greeting" >
+<div align="center html2canvas-ignore">
+     <div style="font-size: 17px; font-weight: 800; color: white;" >
+<center>
+    <p id="demo"></p></center>
+
+            <div class="main_body" >
+<center>
+<figure>
+    
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+
+
+</figure><br> <br><br><br>
+</center>
+<p id="demo"></p> 
+<center>
+<img src="img/wish.webp" width="70%" height="13%"style="animation: tada 4s infinite"><br>
+<img src="img/mc.webp" width="300px" height="400px"style="animation: pulse 1.5s infinite"><br><br>
+</center>
+<center>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:&effect=outline' rel='stylesheet'>
+<span class="font-effect-outline" style="font-size: 18pt; font-family: Open Sans; color: #edff2d;">
+Now Share it to your friends on<br />
+WhatsApp, Facebook, & Other Places<br />
+So that People can see your Name and Wishes sent by You.<br />
+</pre>
+<br />
+</center>
+<center>
+<img src="img/c.webp"  class="swing2"  height="340px" width="300px"/>
+</center>
+            
+
+   <!-- Display the countdown timer in an element -->
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 25, 2020 00:00:00").getTime();
+
+// Update the count down every 01 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "<i> Days,</i> " + hours + " <i>Hrs,</i> "
+  + minutes + "<i> Min,</i> " + seconds + "<i> Sec</i> ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Merry Christmas";
+  }
+}, 1000);
+</script> 
+</div>
+
+<center>
+<div class="button">
+    
+                
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</div></center><br>
+
+<center>
+
+
+
+</center>
+
+
+<br><br>
+</div>
+</div>
+<center>
+<a class="footerbtn" href="whatsapp://send?text=🔥 *Merry Christmas* 
+ 🎄*XMas Gift for You🎄* *Click Below Link and See Magic!🎁*     
+ 👉👉christmas.sarkarispy.com/?n=<?php echo str_replace(" ","%2B",$_GET["n"]); ?>"> <img width="25px" height="25px" src="https://goo.gl/AejrnN"/><b style="font-size: 20px;">Share on Whatsapp</b> <img width="25px" height="25px" src="https://goo.gl/AejrnN"/></a>
+</center>
+
+    </body>
+</html> 
+
